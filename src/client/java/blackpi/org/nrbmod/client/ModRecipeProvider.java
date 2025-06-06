3f240e177998cfe2e4f2e42e5649cfbcc73f523e
Josue Ramos <54600529+nullr00tbyte@users.noreply.github.com>
2025-06-05 20:02:23 -0600

    refactor: unused imports

--- src/client/java/blackpi/org/nrbmod/client/ModRecipeProvider.java
+++ src/client/java/blackpi/org/nrbmod/client/ModRecipeProvider.java
@@ -1,14 +1,10 @@
 package blackpi.org.nrbmod.client;
-
 import java.util.concurrent.CompletableFuture;
-
 import blackpi.org.rootkz.item.ModItems;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
-import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
